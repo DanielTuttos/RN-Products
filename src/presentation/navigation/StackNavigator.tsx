8f8f7e0c7ed527498081e4ecc8a2b4e67a027893
Daniel Romero <tutorialesdanielromero@gmail.com>
2024-04-11 14:04:16 -0500

    :construction: NAVIGATION

---
+++ src/presentation/navigation/StackNavigator.tsx
@@ -0,0 +1,33 @@
+import {createStackNavigator} from '@react-navigation/stack';
+import {
+  HomeScreen,
+  LoadingScreen,
+  LoginScreen,
+  ProductScreen,
+  RegisterScreen,
+} from '../screens';
+
+export type RootStackParams = {
+  LoadingScreen: undefined;
+  LoginScreen: undefined;
+  RegisterScreen: undefined;
+  HomeScreen: undefined;
+  ProductScreen: {productId: string};
+};
+
+const Stack = createStackNavigator<RootStackParams>();
+
+export const StackNavigator = () => {
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        headerShown: false,
+      }}>
+      <Stack.Screen name="LoadingScreen" component={LoadingScreen} />
+      <Stack.Screen name="LoginScreen" component={LoginScreen} />
+      <Stack.Screen name="RegisterScreen" component={RegisterScreen} />
+      <Stack.Screen name="HomeScreen" component={HomeScreen} />
+      <Stack.Screen name="ProductScreen" component={ProductScreen} />
+    </Stack.Navigator>
+  );
+};
